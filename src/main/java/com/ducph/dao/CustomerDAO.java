b4124289218b41e7af837ebaf0cfd0105dbb1f81
hongducphan <hongducphan912@gmail.com>
2019-12-23 21:05:41 +0700

    Add mvc code

---
+++ src/main/java/com/ducph/dao/CustomerDAO.java
@@ -0,0 +1,18 @@
+package com.ducph.dao;
+
+
+import com.ducph.entity.Customer;
+
+import java.util.List;
+
+public interface CustomerDAO {
+
+    List<Customer> getCustomers();
+
+    void saveCustomer(Customer theCustomer);
+
+    Customer getCustomer(int theId);
+
+    void deleteCustomer(int theId);
+
+}
